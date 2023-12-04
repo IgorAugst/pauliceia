21b8349ef5d15270c2b0ec8cb447bc7885c49e64
Igor Augusto <igoraugusto1029@gmail.com>
2023-12-03 22:57:40 -0300

    altera config

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,10 +5,10 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   // urlVGI: '"http://localhost:3001"',
-  urlVGI: '"http://www.pauliceia.dpi.inpe.br/api/vgi"',
+  urlVGI: '"https://pauliceia.unifesp.br/api/vgi"',
   // urlGeoserverPauliceia: '"http://localhost:9021/geoserver/pauliceia"',
-  urlGeoserverPauliceia: '"http://www.pauliceia.dpi.inpe.br/geoserver/pauliceia"',
-  urlGeoserveOther: '"http://www.pauliceia.dpi.inpe.br/geoserver/other"',
-  urlGeocoding: '"http://www.pauliceia.dpi.inpe.br/api/geocoding"',
+  urlGeoserverPauliceia: '"https://pauliceia.unifesp.brgeoserver/pauliceia"',
+  urlGeoserveOther: '"https://pauliceia.unifesp.br/geoserver/other"',
+  urlGeocoding: '"http://localhost:3000/api/geocoding"',
   keyCripto: '"keytest"'
 })
